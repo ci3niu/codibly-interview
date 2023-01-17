283ba58176bce86cf5814bfd98a58ed0a2f845ea
Cieniu <em.cieniek@gmail.com>
2023-01-17 09:34:14 +0100

    font fixes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
+const theme = extendTheme({
+  styles: {
+    global: {
+      'html, body': {
+        color: 'black'
+      }
+    }
+  }
+});
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <ChakraProvider>
+  <ChakraProvider theme={theme}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
